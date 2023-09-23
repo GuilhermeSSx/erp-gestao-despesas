afa426c0d5c9fdc9e89c2ac0a493a904f90ac647
GuilhermeSSx <guilhermedosantos45@gmail.com>
2023-09-23 03:56:35 -0300

    504

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
+import NextAuth, { NextAuthOptions } from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
 
 const nextAuthOptions: NextAuthOptions = {
 	providers: [
@@ -11,18 +11,7 @@ const nextAuthOptions: NextAuthOptions = {
 			},
 
 			async authorize(credentials, req) {
-				// Defina o tempo limite em milissegundos (por exemplo, 10 segundos)
-				const timeoutMilliseconds = 10000; // 10 segundos
-
-				// Crie uma Promise que irá resolver após o tempo limite
-				const timeoutPromise = new Promise<Response>((_, reject) => {
-					setTimeout(() => {
-						reject(new Error("Tempo limite excedido"));
-					}, timeoutMilliseconds);
-				});
-
-				// Crie uma Promise para a chamada da API
-				const apiCallPromise = fetch('https://jpnr-gestao-api.vercel.app/user/sign-in', {
+				const response = await fetch('https://jpnr-gestao-api.vercel.app/user/sign-in', {
 					method: 'POST',
 					headers: {
 						'Content-type': 'application/json'
@@ -31,23 +20,16 @@ const nextAuthOptions: NextAuthOptions = {
 						email: credentials?.email,
 						password: credentials?.password
 					})
-				});
+				})
 
-				try {
-					// Use Promise.race para executar a chamada da API com um tempo limite
-					const response: Response = await Promise.race([apiCallPromise, timeoutPromise]);
+				console.log('API Response Status Code:', response.status);
 
-					console.log('API Response Status Code:', response.status);
-
-					if (response.ok) {
-						const user = await response.json();
-						console.log('API Response JSON Data:', user);
-						if (user) {
-							return user;
-						}
+				if (response.ok) {
+					const user = await response.json();
+					console.log('API Response JSON Data:', user);
+					if (user) {
+						return user;
 					}
-				} catch (error: any) {
-					console.error('Erro na chamada da API:', error.message);
 				}
 
 				return null;
@@ -62,7 +44,7 @@ const nextAuthOptions: NextAuthOptions = {
 			user && (token.user = user)
 			return token
 		},
-		async session({ session, token }) {
+		async session({ session, token }){
 			session = token.user as any
 			return session
 		}
