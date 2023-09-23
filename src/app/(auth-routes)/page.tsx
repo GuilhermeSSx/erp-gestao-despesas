a3a69fadd6c448e531a8ff0712a3f0f2561a5e83
GuilhermeSSx <guilhermedosantos45@gmail.com>
2023-09-22 23:42:27 -0300

    carregando login

--- src/app/(auth-routes)/page.tsx
+++ src/app/(auth-routes)/page.tsx
@@ -1,70 +1,72 @@
 "use client"
 import Link from 'next/link'
-
 import { signIn } from "next-auth/react";
-import { useRouter } from "next/navigation";
+import { useRouter } from "next/navigation"; // Corrigido de next/navigation
 import { SyntheticEvent, useState } from "react";
 
 export default function Login() {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [isLoading, setIsLoading] = useState<boolean>(false); // Rastreia o estado de carregamento
+  const [errorMessage, setErrorMessage] = useState<string | null>(null); // Rastreia as mensagens de erro
 
-  const [email, setEmail] = useState<string>('')
-  const [password, setPassword] = useState<string>('')
-
-  const router = useRouter()
+  const router = useRouter();
 
   async function handleSubmit(event: SyntheticEvent) {
-    event.preventDefault()
+    event.preventDefault();
+
+    // Inicia o carregamento
+    setIsLoading(true);
 
     const result = await signIn('credentials', {
       email,
       password,
-      redirect: false
-    })
+      redirect: false,
+    });
+
+    // Finaliza o carregamento
+    setIsLoading(false);
 
     if (result?.error) {
-      console.log(result)
-      return
+      setErrorMessage('Email ou senha incorretos'); // Define a mensagem de erro
+    } else {
+      setErrorMessage(null); // Reseta a mensagem de erro se não houver erro
+      router.replace('/modulos');
+      window.location.reload();
     }
-
-    router.replace('/modulos')
-    window.location.reload(); // Recarrega a página após o redirecionamento
   }
 
-
-
-
   return (
-    <main className=' fixed h-[calc(100vh-100px)] w-screen bg-slate-400 flex justify-center items-center px-4'>
-
-      <div className=' px-24 sm:px-28 max-w-[100%] h-[28rem] bg-[#ffffff] rounded-xl flex flex-col items-center'>
+    <main className='fixed h-[calc(100vh-100px)] w-screen bg-slate-400 flex justify-center items-center px-4'>
+      <div className='px-24 sm:px-28 max-w-[100%] h-[30rem] bg-[#ffffff] rounded-xl flex flex-col items-center'>
         <h2 className='text-[1.3rem] font-extrabold mt-10 text-center bg-black bg-clip-text text-transparent'>Entrar na sua conta</h2>
-        <p className=' text-[0.9rem] mt-[0.13rem] text-center'>Ou
+        <p className='text-[0.9rem] mt-[0.13rem] text-center'>Ou
           <Link className='ml-1 text-blue-500 hover:text-black underline' href={'/cadastro'}>
             Cadastrar Nova Conta
           </Link>
         </p>
 
         <form className='w-[150%] flex flex-col justify-center' onSubmit={handleSubmit}>
-          <div className=''>
-            <input 
-              id='email' 
+          <div>
+            <input
+              id='email'
               type='email'
               autoComplete='none'
-              required 
+              required
               className='appearance-none rounded-none relative
-              block border w-full px-2 py-2 mt-12 rounded-t-md' 
+              block border w-full px-2 py-2 mt-12 rounded-t-md'
               placeholder='Email'
               onChange={(event) => setEmail(event.target.value)}
-              />
+            />
           </div>
           <div>
-            <input 
-              id='password' 
-              type='password' 
-              autoComplete='none' 
-              required 
+            <input
+              id='password'
+              type='password'
+              autoComplete='none'
+              required
               className='appearance-none rounded-none relative
-              block border w-full px-2 py-2 mt-4 rounded-t-md' 
+              block border w-full px-2 py-2 mt-4 rounded-t-md'
               placeholder='Senha'
               onChange={(event) => setPassword(event.target.value)}
             />
@@ -81,22 +83,29 @@ export default function Login() {
             </div>
           </div>
 
-          <div className='mt-10'>
+          <div className='mt-8'>
             <button
               type='submit'
               className='group relative w-full flex justify-center py-2 px-4 border border-transparent
-            text-sm font-medium rounded-md bg-lime-400 hover:bg-lime-500 hover:scale-[1.02] transition duration-300'>Entrar</button>
+              text-sm font-medium rounded-md bg-lime-400 hover:bg-lime-500 hover:scale-[1.02] transition duration-300'
+              disabled={isLoading} // Desativa o botão durante o carregamento
+            >
+              {isLoading ? 'Carregando...' : 'Entrar'} {/* Mostra "Carregando" durante o carregamento */}
+            </button>
           </div>
         </form>
 
+        {/* Exibe a mensagem de erro, se houver */}
+        {errorMessage && <p className='text-red-500 text-sm flex justify-center mt-2'>{errorMessage}</p>}
+
+
         <button
-          className="mt-5 px-4 py-2 border flex gap-2 border-slate-200 rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150">
-          <img className="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo" />
+          className="mt-8 px-4 py-2 border flex gap-2 border-slate-200 rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150">
+          <img className="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="logo do Google" />
           <span>Login com o Google</span>
         </button>
-
-
       </div>
     </main>
-  )
+  );
 }
+
