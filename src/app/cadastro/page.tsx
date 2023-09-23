de5d399c8d345507467f3dc37e411188d85d08cc
GuilhermeSSx <guilhermedosantos45@gmail.com>
2023-09-23 04:43:34 -0300

    cadastro

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -1,47 +1,116 @@
-import Image from 'next/image'
+"use client"
+import { useState } from 'react';
+import { useRouter } from 'next/navigation'; // Importe o useRouter para lidar com a navegação
 
 export default function Cadastro() {
+    const [formData, setFormData] = useState({
+        nome: '',
+        email: '',
+        password: '',
+    });
+
+    const [loading, setLoading] = useState(false);
+    const [cadastroSuccess, setCadastroSuccess] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const router = useRouter(); // Inicialize o useRouter
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setLoading(true);
+
+        try {
+            const response = await fetch('https://jpnr-gestao-api.vercel.app/user/sign-up', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+
+            if (response.ok) {
+                setCadastroSuccess(true);
+            } else {
+                const errorData = await response.json();
+                setErrorMessage(errorData.error || 'Erro no cadastro.');
+            }
+        } catch (error) {
+            console.error('Erro no cadastro:', error);
+            setErrorMessage('Erro no cadastro.');
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const handleVoltarParaLogin = () => {
+        router.push('/'); // Redirecione para a página de login quando o botão "Voltar para o Login" for clicado
+    };
+
     return (
-        <main className=' fixed h-[calc(100vh-100px)] w-screen bg-slate-400 flex justify-center items-center px-4'>
-
-            <div className=' px-24 sm:px-28 max-w-[100%] h-[28rem] bg-[#ffffff] rounded-xl flex flex-col items-center'>
-                <h2 className='text-[1.3rem] font-extrabold mt-8 text-center'>Criar sua Conta</h2>
-
-                <button
-                    className="mt-4 px-4 py-2 border flex gap-2 border-slate-200 rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150">
-                    <img className="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo" />
-                    <span>Cadastro com o Google</span>
-                </button>
-
-                <form className='w-[150%] flex flex-col justify-center'>
+        <main className='fixed h-[calc(100vh-100px)] w-screen bg-slate-400 flex justify-center items-center px-4'>
+            <div className='px-24 sm:px-28 max-w-[100%] h-[30rem] bg-[#ffffff] rounded-xl flex flex-col items-center'>
+                <h2 className='text-[1.3rem] font-extrabold mt-12 text-center'>Criar sua Conta</h2>
+                <form className='w-[150%] flex flex-col justify-center' onSubmit={handleSubmit}>
                     <div className=''>
-                        <input id='nome' type='text' autoComplete='none' required className='appearance-none rounded-none relative
-                            block border w-full px-3 py-2 mt-6 rounded-t-md' placeholder='Nome' />
+                        <input
+                            name='nome'
+                            type='text'
+                            autoComplete='none'
+                            required
+                            className='appearance-none rounded-none relative block border w-full px-3 py-2 mt-6 rounded-t-md'
+                            placeholder='Nome'
+                            onChange={handleChange}
+                        />
                     </div>
                     <div className=''>
-                        <input id='email' type='email' autoComplete='none' required className='appearance-none rounded-none relative
-                            block border w-full px-3 py-2 mt-4 rounded-t-md' placeholder='Email' />
+                        <input
+                            name='email'
+                            type='email'
+                            autoComplete='none'
+                            required
+                            className='appearance-none rounded-none relative block border w-full px-3 py-2 mt-4 rounded-t-md'
+                            placeholder='Email'
+                            onChange={handleChange}
+                        />
                     </div>
                     <div>
-                        <input id='password' type='password' autoComplete='none' required className='appearance-none rounded-none relative
-                            block border w-full px-3 py-2 mt-4 rounded-t-md' placeholder='Senha' />
+                        <input
+                            name='password'
+                            type='password'
+                            autoComplete='none'
+                            required
+                            className='appearance-none rounded-none relative block border w-full px-3 py-2 mt-4 rounded-t-md'
+                            placeholder='Senha'
+                            onChange={handleChange}
+                        />
                     </div>
-                    <div className="flex h-fit items-center justify-center mt-10">
-                        <div className="group relative flex-1">
-                            <div className=" absolute -inset-1 rounded-lg bg-gradient-to-r from-lime-500 via-gray-200 to-gray-400 opacity-30 blur transition duration-500 group-hover:opacity-100"></div>
-                            <button className=" shadow-lg w-full relative bg-lime-300 rounded-lg  px-7 py-4 text-black">Cadastrar</button>
+                    <div className='flex h-fit items-center justify-center mt-10'>
+                        <div className='group relative flex-1'>
+                            <div className='absolute -inset-1 rounded-lg bg-gradient-to-r from-lime-500 via-gray-200 to-gray-400 opacity-30 blur transition duration-500 group-hover:opacity-100'></div>
+                            <button
+                                className='shadow-lg w-full relative bg-lime-300 rounded-lg  px-7 py-4 text-black'
+                                disabled={loading}
+                            >
+                                {loading ? 'Carregando...' : 'Cadastrar'}
+                            </button>
                         </div>
                     </div>
-
-                    {/* <div className='mt-10'>
-                        <button className='group relative w-full flex justify-center py-2 px-4 border border-transparent
-                            text-sm font-medium rounded-md bg-lime-400 hover:bg-lime-500'>Cadastrar</button>
-                    </div> */}
+                    {cadastroSuccess && <p className='text-green-500 mt-4 flex justify-center'>Cadastro realizado com sucesso!</p>}
+                    {errorMessage && <p className='text-red-500 mt-4'>{errorMessage}</p>}
+                    <button
+                        type='button' // Defina o tipo como 'button' para evitar envio do formulário
+                        className='mt-4 text-blue-500 hover:underline cursor-pointer'
+                        onClick={handleVoltarParaLogin}
+                    >
+                        Voltar para o Login
+                    </button>
                 </form>
-
-
-
             </div>
         </main>
-    )
+    );
 }
