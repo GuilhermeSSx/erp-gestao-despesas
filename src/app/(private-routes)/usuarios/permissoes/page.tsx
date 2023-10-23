04b87eced9635b1e773fdfb318e17f66b877e657
GuilhermeSSx <guilhermedosantos45@gmail.com>
2023-10-23 03:45:08 -0300

    cadastro, seleção feitos -> fazer excluirPerfilAcesso

--- src/app/(private-routes)/usuarios/permissoes/page.tsx
+++ src/app/(private-routes)/usuarios/permissoes/page.tsx
@@ -1,20 +1,111 @@
 "use client"
 import TablePerfisAcesso from '@/app/components/tablePerfisAcesso';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { UserPlusIcon } from '@heroicons/react/20/solid';
+import { toast } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 interface PerfilAcesso {
-    id: number;
-    name: string;
+    id_perfil_acesso: number;
+    nome_perfil_acesso: string;
 }
 
-const perfisAcessos: PerfilAcesso[] = [
-    { id: 1, name: 'Admin' },
-    { id: 2, name: 'Convidado' },
-    { id: 3, name: 'funcionario' },
-];
-
 const CriarSelecionarPerfilAcesso = () => {
+
+    const [perfilAcessosData, setPerfilAcessosData] = useState<PerfilAcesso[]>([]);
+
+    useEffect(() => {
+        getPerfilAcessos();
+    }, []);
+
+    const getPerfilAcessos = async () => {
+        try {
+            const response = await fetch('https://jpnr-gestao-sqlserver.vercel.app/user/get-perfil-acessos', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                setPerfilAcessosData(data.perfil_acessos);
+
+            } else {
+                throw new Error('Erro ao buscar os perfis de acessos.');
+            }
+        } catch (error) {
+            console.error('Erro:', error);
+        }
+    };
+
+    //-------------------------------------------------------------------------------
+
+    const [loading, setLoading] = useState(false);
+
+    const [formData, setFormData] = useState({
+        nome_perfil_acesso: ''
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setLoading(true);
+
+        try {
+            const response = await fetch('https://jpnr-gestao-sqlserver.vercel.app/user/criar-perfil-acesso', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+
+            if (response.ok) {
+                getPerfilAcessos();
+
+                toast.success('Cadastro Realizado com sucesso', {
+                    position: 'bottom-left',
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: 'light',
+                });
+            } else {
+                toast.error('Erro em cadastrar o usuário', {
+                    position: 'bottom-left',
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: 'light',
+                });
+            }
+        } catch (error) {
+            toast.error('Erro contate o administrador!', {
+                position: 'bottom-left',
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: 'light',
+            });
+        } finally {
+            setLoading(false);
+        }
+    };
 
     const [selectedPefilAcesso, setSelectedPerfilAcesso] = useState<PerfilAcesso | null>(null);
 
@@ -24,42 +115,50 @@ const CriarSelecionarPerfilAcesso = () => {
     };
 
     return (
-        <main className='md-web:w-screen w-full h-[calc(100vh-60px)] flex justify-center items-center p-2 flex-col overflow-auto'>
+        <main className='fixed w-screen h-[calc(100vh-60px)] flex justify-center'>
 
-            <section className='w-full h-full md-web:w-[60%] md:w-[40%] flex flex-col justify-center items-center'>
-                <div className='w-full flex flex-col items-center  p-3 rounded-xl  '>
-                    <div className='rounded-lg w-full p-1 '>
+            <div className='w-full h-full md-web:w-[70%] md:w-[50%]'>
+
+                <form className='w-full h-fit flex flex-col items-center rounded-xl mt-8' onSubmit={handleSubmit}>
+                    <div className='rounded-lg w-full'>
                         <h2 className="text-xl font-bold text-center">Criar perfil de acesso</h2>
                         <div className="border mt-4" />
                         <input
                             id='nome-perfil-acesso'
+                            name='nome_perfil_acesso'
                             className='mt-3 relative block border-2 w-full px-4 py-2 rounded-t-md'
                             placeholder='Nome do perfil de acesso...'
                             type='text'
-                        // value={}
-                        // onChange={(e) => setInputValue(e.target.value)}
+                            required
+                            onChange={handleChange}
+                            maxLength={15}
+                            minLength={4}
                         />
                     </div>
 
-                    <div className='flex justify-between mt-4 w-full'>
-                        <button title="Salvar Favorecido" className='mt-2 group relative w-full flex justify-center items-center py-1 px-4 border border-transparent
-                                        text-base rounded-md bg-emerald-400 hover:bg-lime-500 '>
-                            Cadastrar
+                    <div className='flex justify-between mt-2 w-full'>
+                        <button
+                            title="Cadastrar Perfil de Acesso"
+                            className='mt-2 group relative w-full flex justify-center items-center py-1 px-4 border border-transparent
+                            text-base rounded-md bg-emerald-400 hover:bg-lime-500'
+                            disabled={loading}
+                        >
+                            {loading ? 'Carregando...' : 'Cadastrar'}
                             <UserPlusIcon
                                 className="ml-2 h-7 w-5 text-center"
                                 aria-hidden="true"
                             />
                         </button>
                     </div>
+                </form>
+
+                <div className='mt-4 rounded-xl flex h-[calc(100vh-300px)]'>
+                    <TablePerfisAcesso perfisAcessos={perfilAcessosData} onPefilAcessoSelected={handlePefilAcessoSelected} />
                 </div>
-
-                <div className='mt-8 w-full flex flex-col items-center p-3 rounded-xl'>
-                    <TablePerfisAcesso perfisAcessos={perfisAcessos} onPefilAcessoSelected={handlePefilAcessoSelected} />
-
-                </div>
+                
 
 
-            </section>
+            </div>
 
 
         </main>
--- src/app/components/tablePerfisAcesso/index.tsx
+++ src/app/components/tablePerfisAcesso/index.tsx
@@ -1,9 +1,10 @@
 import Link from 'next/link';
 import React, { useState, useEffect, useRef } from 'react';
+import PopupExcluirPerfilAcesso from '../popupExclurPerfilAcesso';
 
 interface PerfilAcesso {
-    id: number;
-    name: string;
+    id_perfil_acesso: number;
+    nome_perfil_acesso: string;
 }
 
 interface TablePerfisAcessosProps {
@@ -13,6 +14,7 @@ interface TablePerfisAcessosProps {
 
 const TablePerfisAcesso: React.FC<TablePerfisAcessosProps> = ({ perfisAcessos, onPefilAcessoSelected }) => {
     const [selectedItemIndex, setSelectedItemIndex] = useState<number | null>(null);
+    const [selectedItem, setSelectedItem] = useState<PerfilAcesso | null>(null);
     const [selectedEditarItem, setSelectedEditarItem] = useState<PerfilAcesso | null>(null);
     const tableRef = useRef<HTMLTableElement | null>(null);
 
@@ -61,11 +63,22 @@ const TablePerfisAcesso: React.FC<TablePerfisAcessosProps> = ({ perfisAcessos, o
         onPefilAcessoSelected(perfilAcesso);
     };
 
+    const [popupAbertoExcluirPerfilAcesso, setPopupAbertoExcluirPerfilAcesso] = useState(false);
+
+    const abrirPopupExcluirPerfilAcesso = (perfil_acesso: PerfilAcesso) => {
+        setSelectedItem(perfil_acesso);
+        setPopupAbertoExcluirPerfilAcesso(true);
+    };
+
+    const fecharPopupExcluirPerfilAcesso = () => {
+        setPopupAbertoExcluirPerfilAcesso(false);
+    };
+
 
 
     return (
         <div className='rounded-lg border h-full w-[100%] overflow-y-scroll mt-2 bg-white'>
-            <table className="w-full h-fit select-none " ref={tableRef}>
+            <table className="w-full h-fit select-none" ref={tableRef}>
                 <thead className="bg-gray-50 border-b-2 border-gray-200 sticky top-0">
                     <tr className='divide-x divide-gray-300'>
                         <th className="p-3 text-sm font-bold tracking-wide text-left">Perfil de Acesso</th>
@@ -75,18 +88,18 @@ const TablePerfisAcesso: React.FC<TablePerfisAcessosProps> = ({ perfisAcessos, o
                 <tbody className="divide-y-2 divide-blue-100">
                     {perfisAcessos.map((perfilAcesso, index) => (
                         <tr
-                            key={perfilAcesso.id}
+                            key={perfilAcesso.id_perfil_acesso}
                         >
 
                             <td className='w-[50%] p-3 px-4 text-xs font-semibold text-gray-700 whitespace-nowrap'>
                                 <div>
-                                    <h2 className='font-semibold text-gray-500'>{perfilAcesso.name}</h2>
+                                    <h2 className='font-semibold text-gray-500'>{perfilAcesso.nome_perfil_acesso}</h2>
                                 </div>
                             </td>
                             <td className='text-sm text-gray-700 whitespace-nowrap h-[46px] px-1 justify-evenly items-center select-none'>
                                 <div className='flex justify-evenly items-center'>
                                     <Link
-                                        href={{ pathname: "/usuarios/perfil-acesso", query: { id: perfilAcesso.id } }}
+                                        href={{ pathname: "/usuarios/perfil-acesso", query: { id: perfilAcesso.id_perfil_acesso } }}
                                         className='w-[50%] h-[38px] flex items-center justify-center border border-transparent
                                                 text-sm rounded-md bg-blue-400 hover:bg-blue-300'
                                     >
@@ -97,6 +110,7 @@ const TablePerfisAcesso: React.FC<TablePerfisAcessosProps> = ({ perfisAcessos, o
                                         text-sm rounded-md bg-red-300 hover:bg-red-400'
                                         onClick={(e) => {
                                             e.stopPropagation();
+                                            abrirPopupExcluirPerfilAcesso(perfilAcesso);
 
                                         }}
                                     >
@@ -108,6 +122,14 @@ const TablePerfisAcesso: React.FC<TablePerfisAcessosProps> = ({ perfisAcessos, o
                     ))}
                 </tbody>
             </table>
+            {/* <PopupExcluirPerfilAcesso
+                open={popupAbertoExcluirPerfilAcesso}
+                onClose={fecharPopupExcluirPerfilAcesso}
+                id_perfil_acesso={selectedItem ? selectedItem.id_perfil_acesso : null}
+                nome_perfil_acesso={selectedItem ? selectedItem.nome_perfil_acesso : ''}
+                //reloadPerfilAcessos={reloadPerfilAcessos} -- Continuar
+            /> */}
+
         </div>
     );
 };
