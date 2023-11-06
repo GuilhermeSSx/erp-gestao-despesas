5c1fef89a424375c47aa956a9e16e24ef688678b
GuilhermeSSx <guilhermedosantos45@gmail.com>
2023-11-06 14:30:25 -0300

    desativado analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Header from './components/header'
 import NextAuthSessionProvider from '@/providers/sessionProvider'
-import { Analytics } from '@vercel/analytics/react';
+// import { Analytics } from '@vercel/analytics/react';
 import { ToastContainer } from 'react-toastify';
 
 const inter = Inter({ subsets: ['latin'] })
@@ -26,7 +26,7 @@ export default function RootLayout({
           {children}
           <ToastContainer />
         </NextAuthSessionProvider>
-        <Analytics />
+        {/* <Analytics /> */}
       </body>
     </html>
   )
