7db06b1ee31e7f0cebc0a4542e3c1e96d16eeff9
GuilhermeSSx <guilhermedosantos45@gmail.com>
2023-11-16 00:25:19 -0300

    fix 2 test

--- src/app/teste/button.tsx
+++ src/app/teste/button.tsx
@@ -1,47 +1,48 @@
-"use client"
-import React, { useState, useRef } from 'react';
-import PopupExcluirPerfilAcesso from '@/app/components/popupExclurPerfilAcesso';
+// "use client"
+// import React, { useState, useRef } from 'react';
+// import PopupExcluirPerfilAcesso from '@/app/components/popupExclurPerfilAcesso';
 
-interface PerfilAcesso {
-    id_perfil_acesso: number;
-    nome_perfil_acesso: string;
-}
+// interface PerfilAcesso {
+//     id_perfil_acesso: number;
+//     nome_perfil_acesso: string;
+// }
 
-interface ButtonRemoverProps {
-    id_perfil_acesso: number;
-    nome_perfil_acesso: string;
-    // Inclua quaisquer outras props necessárias aqui
-}
+// interface ButtonRemoverProps {
+//     id_perfil_acesso: number;
+//     nome_perfil_acesso: string;
+//     // Inclua quaisquer outras props necessárias aqui
+// }
 
-export const ButtonRemover = ({ id_perfil_acesso, nome_perfil_acesso }: ButtonRemoverProps) => {
+// export const ButtonRemover = ({ id_perfil_acesso, nome_perfil_acesso }: ButtonRemoverProps) => {
 
-    const [itemToBeRemoved, setItemToBeRemoved] = useState<PerfilAcesso | null>(null);
+//     const [itemToBeRemoved, setItemToBeRemoved] = useState<PerfilAcesso | null>(null);
 
-    const abrirPopupExcluirPerfilAcesso = (perfil_acesso: PerfilAcesso) => {
-        setItemToBeRemoved(perfil_acesso);
-    };
+//     const abrirPopupExcluirPerfilAcesso = (perfil_acesso: PerfilAcesso) => {
+//         setItemToBeRemoved(perfil_acesso);
+//     };
 
-    const fecharPopupExcluirPerfilAcesso = () => {
-        setItemToBeRemoved(null);
-    };
+//     const fecharPopupExcluirPerfilAcesso = () => {
+//         setItemToBeRemoved(null);
+//     };
 
-    return (
-        <>
-            <button
-                className='w-[48%] h-[38px] flex items-center justify-center border border-transparent text-sm rounded-md bg-red-300 hover-bg-red-400'
-                onClick={(e) => {
-                    e.stopPropagation();
-                    abrirPopupExcluirPerfilAcesso( { id_perfil_acesso, nome_perfil_acesso } );
-                }}
-            >
-                Remover
-            </button>
-            <PopupExcluirPerfilAcesso
-                open={itemToBeRemoved !== null}
-                onClose={fecharPopupExcluirPerfilAcesso}
-                id_perfil_acesso={itemToBeRemoved ? itemToBeRemoved.id_perfil_acesso : 0}
-                nome_perfil_acesso={itemToBeRemoved ? itemToBeRemoved.nome_perfil_acesso : ''}
-            />
-        </>
-    )
-}
+//     return (
+//         <>
+//             <button
+//                 className='w-[48%] h-[38px] flex items-center justify-center border border-transparent text-sm rounded-md bg-red-300 hover-bg-red-400'
+//                 onClick={(e) => {
+//                     e.stopPropagation();
+//                     abrirPopupExcluirPerfilAcesso( { id_perfil_acesso, nome_perfil_acesso } );
+//                 }}
+//             >
+//                 Remover
+//             </button>
+//             <PopupExcluirPerfilAcesso
+//                 open={itemToBeRemoved !== null}
+//                 onClose={fecharPopupExcluirPerfilAcesso}
+//                 id_perfil_acesso={itemToBeRemoved ? itemToBeRemoved.id_perfil_acesso : 0}
+//                 nome_perfil_acesso={itemToBeRemoved ? itemToBeRemoved.nome_perfil_acesso : ''}
+
+//             />
+//         </>
+//     )
+// }
