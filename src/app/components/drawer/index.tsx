6041faf3243e4c970ef11733b81e0654c70f834c
GuilhermeSSx <guilhermedosantos45@gmail.com>
2023-11-23 04:13:21 -0300

    adicionado marcação de rota no drawer, falta abrir o menu do drawer e em perfil de acesso manter selecionado ao mudar de rota para modulos e funcionalidades

--- src/app/components/drawer/index.tsx
+++ src/app/components/drawer/index.tsx
@@ -3,11 +3,13 @@ import { motion, AnimatePresence } from 'framer-motion';
 import { ChevronDownIcon, Bars3Icon } from "@heroicons/react/20/solid";
 import { Menu } from "@headlessui/react";
 import Link from "next/link";
-import { type } from 'os';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
 
 function DrawerOpenClose() {
     const [isOpen, setIsOpen] = useState(false);
     const drawerRef = useRef<HTMLDivElement | null>(null);
+    const pathname = usePathname();
 
     useEffect(() => {
         const handleClick = (event: MouseEvent) => {
@@ -17,20 +19,24 @@ function DrawerOpenClose() {
         };
 
         if (isOpen) {
-            // Adicione um ouvinte de clique global para fechar o drawer quando o usuário clicar fora dele.
             window.addEventListener('click', handleClick);
         }
 
         return () => {
-            // Remova o ouvinte de clique ao desmontar o componente.
             window.removeEventListener('click', handleClick);
         };
     }, [isOpen]);
 
     const toggleDrawer = (event: React.MouseEvent) => {
-        event.stopPropagation(); // Impede que o evento se propague para o elemento pai.
+        event.stopPropagation();
         setIsOpen(!isOpen);
     };
+
+    const linkClassName = (href: string) =>
+        clsx(
+            'font-semibold flex w-full items-center rounded-md p-4 text-sm hover:bg-slate-200',
+            { 'bg-selecaoLinha': pathname.startsWith(href) }
+        );
 
     return (
         <>
@@ -43,7 +49,6 @@ function DrawerOpenClose() {
             <AnimatePresence>
                 {isOpen && (
                     <>
-
                         <motion.div
                             initial={{ opacity: 0 }}
                             animate={{ opacity: 1 }}
@@ -71,105 +76,64 @@ function DrawerOpenClose() {
                                     </button>
                                 </div>
 
-                                <Menu
-                                    as="div"
-                                    id="MenuDiv"
-                                    className="relative p-3 bg-[#6964643d]"
-                                >
-
-                                    <Menu.Button
-                                        id="MenuButton"
-                                        className="relavite z-24 inline-flex w-full justify-between rounded-md bg-white bg-opacity-30 px-4 py-[10px] text-sm font-medium text-white hover:bg-opacity-80 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-90"
-                                    >
-                                        <span className=" md-web:flex text-black font-bold text-base">Modulos</span>
-
-                                        <ChevronDownIcon
-                                            className="ml-2 -mr-1 h-5 w-5 text-black hover:text-[#CE466F]"
-                                            aria-hidden="true"
-                                        />
+                                <Menu as="div" className="relative p-3 bg-[#6964643d]">
+                                    <Menu.Button className="relative z-24 inline-flex w-full justify-between rounded-md bg-white bg-opacity-30 px-4 py-[10px] text-sm font-medium text-white hover:bg-opacity-80 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-90">
+                                        <span className="md-web:flex text-black font-bold text-base">Módulos</span>
+                                        <ChevronDownIcon className="ml-2 -mr-1 h-5 w-5 text-black hover:text-[#CE466F]" aria-hidden="true" />
                                     </Menu.Button>
 
                                     <Menu.Items className="right-0 mt-2 w-full origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                        <motion.div
-                                            className="px-1 py-1"
-                                            initial={{ opacity: 0, y: -150 }}
-                                            animate={{ opacity: 1, y: 0 }}
-                                            exit={{ opacity: 0, y: -150 }}
-                                        >
-                                            <Menu.Item>
-                                                <>
-                                                    <Link
-                                                        onClick={toggleDrawer}
-                                                        className={"font-semibold group flex w-full items-center rounded-md p-4 text-sm hover:bg-selecaoLinha"}
-                                                        href={"/modulos/cadastros"}
-                                                    >
-                                                        Cadastros
-                                                    </Link>
-                                                    <Link
-                                                        onClick={toggleDrawer}
-                                                        className={"font-semibold group flex w-full items-center rounded-md p-4 text-sm hover:bg-selecaoLinha"}
-                                                        href={"/modulos/lancamentos"}
-                                                    >
-                                                        Lançamentos
-                                                    </Link>
-                                                </>
-                                            </Menu.Item>
-                                        </motion.div>
+                                        <Menu.Item as="div">
+                                            <Link
+                                                onClick={toggleDrawer}
+                                                className={linkClassName("/modulos/cadastros")}
+                                                href="/modulos/cadastros"
+                                            >
+                                                Cadastros
+                                            </Link>
+                                        </Menu.Item>
+                                        <Menu.Item as="div" >
+                                            <Link
+                                                onClick={toggleDrawer}
+                                                className={linkClassName("/modulos/lancamentos")} 
+                                                href="/modulos/lancamentos"
+                                            >
+                                                Lançamentos
+                                            </Link>
+                                        </Menu.Item>
                                     </Menu.Items>
                                 </Menu>
 
-                                <Menu
-                                    as="div"
-                                    id="MenuDiv"
-                                    className="relative p-3 bg-[#6964643d]"
-                                >
-
-                                    <Menu.Button
-                                        id="MenuButton"
-                                        className="relavite z-24 inline-flex w-full justify-between rounded-md bg-white bg-opacity-30 px-4 py-[10px] text-sm font-medium text-white hover:bg-opacity-80 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-90"
-                                    >
-                                        <span className=" md-web:flex text-black font-bold text-base">Configurações</span>
-
-                                        <ChevronDownIcon
-                                            className="ml-2 -mr-1 h-5 w-5 text-black hover:text-[#CE466F]"
-                                            aria-hidden="true"
-                                        />
+                                <Menu as="div" className="relative p-3 bg-[#6964643d]">
+                                    <Menu.Button className="relative z-24 inline-flex w-full justify-between rounded-md bg-white bg-opacity-30 px-4 py-[10px] text-sm font-medium text-white hover:bg-opacity-80 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-90">
+                                        <span className="md-web:flex text-black font-bold text-base">Configurações</span>
+                                        <ChevronDownIcon className="ml-2 -mr-1 h-5 w-5 text-black hover:text-[#CE466F]" aria-hidden="true" />
                                     </Menu.Button>
 
                                     <Menu.Items className="right-0 mt-2 w-full origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                        <motion.div
-                                            className="px-1 py-1"
-                                            initial={{ opacity: 0, y: -150 }}
-                                            animate={{ opacity: 1, y: 0 }}
-                                            exit={{ opacity: 0, y: -150 }}
-                                        >
-                                            <Menu.Item>
-                                                <>
-                                                    <Link
-                                                        onClick={toggleDrawer}
-                                                        className={"font-semibold group flex w-full items-center rounded-md p-4 text-sm hover:bg-selecaoLinha"}
-                                                        href={"/configuracoes/usuarios"}
-                                                    >
-                                                        Usuarios
-                                                    </Link>
-                                                    <Link
-                                                        onClick={toggleDrawer}
-                                                        className={"font-semibold group flex w-full items-center rounded-md p-4 text-sm hover:bg-selecaoLinha"}
-                                                        href={"/configuracoes/perfil-acesso"}
-                                                    >
-                                                        Perfil de Acesso
-                                                    </Link>
-                                                </>
-                                            </Menu.Item>
-                                        </motion.div>
+                                        <Menu.Item as="div">
+                                            <Link
+                                                onClick={toggleDrawer}
+                                                className={linkClassName("/configuracoes/usuarios")}
+                                                href="/configuracoes/usuarios"
+                                            >
+                                                Usuários
+                                            </Link>
+                                        </Menu.Item>
+                                        <Menu.Item as="div" >
+                                            <Link
+                                                onClick={toggleDrawer}
+                                                className={linkClassName("/configuracoes/perfil-acesso")}
+                                                href="/configuracoes/perfil-acesso"
+                                            >
+                                                Perfil de Acesso
+                                            </Link>
+                                        </Menu.Item>
                                     </Menu.Items>
                                 </Menu>
-
-
                             </div>
                         </motion.div>
                     </>
-
                 )}
             </AnimatePresence>
         </>
