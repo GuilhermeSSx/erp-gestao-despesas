00b116139b107d081f94a0e0fcaacd6783300063
GuilhermeSSx <guilhermedosantos45@gmail.com>
2023-11-26 04:59:36 -0300

    fix border basicTabs reload

--- src/app/components/basicTabs/index.tsx
+++ src/app/components/basicTabs/index.tsx
@@ -1,32 +1,44 @@
-"use client"
+"use client";
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
-import React, { useState } from 'react';
 import { motion } from 'framer-motion';
-
 
 export default function BasicTabs() {
     const [value, setValue] = useState(0);
-
     const usuarioParams = useSearchParams();
     const id_perfil_acesso = usuarioParams.get('id');
+    const tab = usuarioParams.get('tab'); // Lendo o parâmetro 'tab'
 
     const buttonVariants = {
         initial: { borderBottom: '2px solid transparent' },
         animate: { borderBottom: '2px solid blue' },
     };
 
-    // Disable buttons for tabs 1 and 2 when id_perfil_acesso is not available
     const isPerfilAcessoAvailable = !!id_perfil_acesso;
+
+    useEffect(() => {
+        // Definindo o valor com base no parâmetro 'tab' da URL
+        switch (tab) {
+            case 'perfil-acesso':
+                setValue(0);
+                break;
+            case 'modulos':
+                setValue(1);
+                break;
+            case 'funcionalidades':
+                setValue(2);
+                break;
+            default:
+                setValue(0); // Caso padrão
+                break;
+        }
+    }, [tab]);
+
 
     return (
         <div className='border-b border-gray-300 flex w-full justify-center box-content md-web:text-sm text-[10px] font-extrabold select-none pt-4'>
-                        <Link href={{
-                pathname: "/configuracoes/perfil-acesso",
-                query: {
-                    id: id_perfil_acesso,
-                },
-            }}>
+            <Link href={{ pathname: "/configuracoes/perfil-acesso", query: { id: id_perfil_acesso, tab: 'perfil-acesso' } }}>
                 <motion.button
                     onClick={() => setValue(0)}
                     className={`select-none tab-button p-4 hover:bg-slate-100 ${value === 0 ? 'active' : ''}`}
@@ -38,12 +50,7 @@ export default function BasicTabs() {
                     PERFIL DE ACESSO
                 </motion.button>
             </Link>
-            <Link href={{
-                pathname: "/configuracoes/modulos",
-                query: {
-                    id: id_perfil_acesso,
-                },
-            }}>
+            <Link href={{ pathname: "/configuracoes/modulos", query: { id: id_perfil_acesso, tab: 'modulos' } }}>
                 <motion.button
                     onClick={() => isPerfilAcessoAvailable && setValue(1)}
                     className={`select-none tab-button p-4 hover:bg-slate-100 ${!isPerfilAcessoAvailable ? ' cursor-not-allowed text-gray-400' : ''} ${value === 1 ? 'active' : ''}`}
@@ -56,12 +63,7 @@ export default function BasicTabs() {
                     MODULOS
                 </motion.button>
             </Link>
-            <Link href={{
-                pathname: "/configuracoes/funcionalidades",
-                query: {
-                    id: id_perfil_acesso,
-                },
-            }}>
+            <Link href={{ pathname: "/configuracoes/funcionalidades", query: { id: id_perfil_acesso, tab: 'funcionalidades' } }}>
                 <motion.button
                     onClick={() => isPerfilAcessoAvailable && setValue(2)}
                     className={`select-none tab-button p-4 hover:bg-slate-100 ${!isPerfilAcessoAvailable ? ' cursor-not-allowed text-gray-400' : ''} ${value === 2 ? 'active' : ''}`}
@@ -74,9 +76,6 @@ export default function BasicTabs() {
                     FUNCIONALIDADES
                 </motion.button>
             </Link>
-
-
-
         </div>
-    )
+    );
 }
